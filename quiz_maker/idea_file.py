45cd977f08ce61ed4aa4dcf267c8c9800ec714c2
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-05-27 15:33:59 +0800

    deleted all the files I created and decided to create an idea_file consisting all features so I can plan how I will separate them into class.

---
+++ quiz_maker/idea_file.py
@@ -0,0 +1,16 @@
+#Quiz Maker Features
+
+#Start message
+#Options
+    #Option one (create new quiz)
+    #Option two (edit existing)
+    #Option three (exit)
+    #Features in both options one and two
+        #input questions and answer
+        #options list
+        #questions with choice and answer
+        # file write
+        # replay
+    #features in all three
+        #Confirmation message
+#Replay
