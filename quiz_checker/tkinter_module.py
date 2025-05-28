59ebd2983d10d82ca1da11d36f6701b830fe0847
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-05-28 18:23:04 +0800

    just copied and pasted the code from the original file into here and added the quiz mechanics too so it will automatically execute the main mechanics of quiz when a file is selected.

---
+++ quiz_checker/tkinter_module.py
@@ -0,0 +1,29 @@
+import tkinter as tk_module
+from tkinter import filedialog
+from quiz_mechanics import quiz
+
+class main_window:
+    def __init__(self):
+        pass
+
+    def main_window(self):
+        choose_file_window = tk_module.Tk()
+        choose_file_window.title("QUIZ CHECKER")
+        choose_file_window.geometry("1000x100") 
+        opening_message = tk_module.Label(choose_file_window, text="QUIZ CHECKER\nThis program is used so you can answer the quiz you " \
+        "just created uisng basic quiz creator. You can also use this so other people can answer your created quiz.\n Note that" \
+        "this will not work if you use other quizzes, not created using basic quiz maker. Otherwise, click the button below to" \
+        "choose which quiz you want to answer.") #Ask user the quiz they want to answer.
+        opening_message.pack()
+
+        choose_quiz_button = tk_module.Button(choose_file_window, text="Choose Quiz", command=self.choose_file)
+        choose_quiz_button.pack()
+
+        choose_file_window.mainloop()
+    
+    def choose_file(self):
+        global chosen_file
+        chosen_file = filedialog.askopenfilename()
+        if chosen_file:
+            chosen_quiz=quiz()
+            chosen_quiz.main_mechanics()
